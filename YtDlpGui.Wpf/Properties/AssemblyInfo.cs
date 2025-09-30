6bca627254de62b01b47336f113f4ec364f5b65a
qzmtch <40316715+qzmtch@users.noreply.github.com>
2025-09-30 20:14:04 +0300

    Create AssemblyInfo.cs

---
+++ YtDlpGui.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YtDlpGui.Wpf")]
+[assembly: AssemblyDescription("Simple yt-dlp GUI (WPF .NET Framework 4.7.2)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YtDlpGui.Wpf")] 
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5E6E5B16-65F7-4C9B-BA3B-5F5B9B0F4B93")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
